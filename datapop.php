7bbd6812ca3a331b1d2ca334ba54201445a58964
Mike Dunton <dunton@psy.fsu.edu>
2013-04-16 21:27:11 +0400

    fixed datapop.php

--- datapop.php
+++ datapop.php
@@ -1,17 +1,18 @@
 <?php 
 //change bard.txt to file name
+$user = "";
+$password = "";
+$dbh = new PDO('mysql:host=localhost; dbname=talkiezone', $user, $password);
 $filename = "bard.txt"; 
 $file = file($filename); 
-$lines = count($file); 
 
-for ($line=0; $line<=$lines; $line++){ 
-
-$final_line = explode(":", $file[$line]); 
-//change bard to class
-mysql_query("INSERT INTO spellbook (name, descr, class, level) VALUES ('$final_line[0]', '$final_line[1]', 'Bard', '$final_line[2]')"); 
-
-echo  ( $line==$lines ) ? "Done" : ""; 
-
-} 
+foreach($file as $line)
+{  
+	$final_line = explode(":", $line); 
+	//change bard to class
+	$dbh->query("INSERT INTO spells (name, description, class, level)".
+               "VALUES ('$final_line[0]', '$final_line[1]', 'Bard', '$final_line[2]')"); 	
+}
+$dbh = null; 
 
 ?>
