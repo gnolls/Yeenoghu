d75982c250f0957a27a1199477a67874b271aeb0
Mike Dunton <dunton@psy.fsu.edu>
2013-04-22 19:50:51 +0400

    Removed public_html folder

--- public_html/forum/cache/tpl_subsilver2_simple_footer.html.php
+++
@@ -1,9 +0,0 @@
-<?php if (!defined('IN_PHPBB')) exit; ?></div>
-
-<div id="wrapfooter">
-	<span class="copyright"><?php echo (isset($this->_rootref['CREDIT_LINE'])) ? $this->_rootref['CREDIT_LINE'] : ''; ?>
-
-</div>
-
-</body>
-</html>
