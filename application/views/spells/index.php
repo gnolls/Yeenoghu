557361802535641908e8fad9240728d5dfbe4ec4
Mike Dunton <dunton@cs.fsu.edu>
2013-04-22 20:06:40 -0400

    semicolons <.<

--- application/views/spells/index.php
+++ application/views/spells/index.php
@@ -3,7 +3,7 @@
 
 <?php
  $imgPath = base_url() ."assets/img/";
- $urlPath = site_url("spells/classes/")
+ $urlPath = site_url("spells/classes/");
  echo anchor($urlPath."bard", $imgPath."bard");
 ?>
 </div>
