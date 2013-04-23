26040de0f3e0b8186aa9332ba6de14259b541925
Mike Dunton <dunton@cs.fsu.edu>
2013-04-22 20:37:31 -0400

    asd

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -14,26 +14,27 @@
 		<nav>
 			<?php echo ul($nav['links'], $nav['attributes']); ?>
 		</nav>
-		<?php if($loginInfo['nLog'] === TRUE) { ?>	
-		<div class="login">
-			<h2>Log in</h2>
-            <?php echo validation_errors(); ?>
-				<form action="http://www.noleptr.com/gnolls/phpBB3/ucp.php?mode=login" method="post">
-				<input type="text" name="username" size="18" placeholder="Username">
-				<input type="password" name="password" size="18" placeholder="Password">
-				<input type="checkbox" name="remember" value="yes" checked="">
-				<input type="hidden" name="autologin" value="on">
-				<input type="hidden" name="redirect" value="/">
-				<button value="Log in" name="login" type="submit">Log in</button>
-				<a href="http://www.noleptr.com/gnolls/phpBB3/ucp.php?mode=sendpassword">Forgotten password?</a>
-				<a href="http://www.noleptr.com/gnolls/phpBB3/ucp.php?mode=register">Register</a>
-				</form> 
-			</div>
-		<?php }else { ?>
-			<div id="login">
-			Welcome back <?php echo $loginInfo['username']; ?>!
-			</div>
-		<?php } ?>
+
     </header>
   </div>	
 <div id="container" class="wrapper">
+	<?php if($loginInfo['nLog'] === TRUE) { ?>	
+<div class="login">
+	<h2>Log in</h2>
+    <?php echo validation_errors(); ?>
+		<form action="http://www.noleptr.com/gnolls/phpBB3/ucp.php?mode=login" method="post">
+		<input type="text" name="username" size="18" placeholder="Username">
+		<input type="password" name="password" size="18" placeholder="Password">
+		<input type="checkbox" name="remember" value="yes" checked="">
+		<input type="hidden" name="autologin" value="on">
+		<input type="hidden" name="redirect" value="/">
+		<button value="Log in" name="login" type="submit">Log in</button>
+		<a href="http://www.noleptr.com/gnolls/phpBB3/ucp.php?mode=sendpassword">Forgotten password?</a>
+		<a href="http://www.noleptr.com/gnolls/phpBB3/ucp.php?mode=register">Register</a>
+		</form> 
+	</div>
+<?php }else { ?>
+	<div class="login">
+	Welcome back <?php echo $loginInfo['username']; ?>!
+	</div>
+<?php } ?>
