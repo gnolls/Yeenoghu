049ac6f6f56dc1c8c1b1c4136b7449a9b63234ae
Michael Dunton <dunton@cs.fsu.edu>
2013-04-24 12:17:31 -0400

    changed jpg to png

--- application/views/spells/index.php
+++ application/views/spells/index.php
@@ -4,15 +4,15 @@
 <?php
  $imgPath = '<img src="'.base_url() .'assets/img/';
  $urlPath = site_url("spells/classes/");
- echo anchor($urlPath."/bard", $imgPath.'bard.jpg" alt="Bard">');
- echo anchor($urlPath."/barbarian", $imgPath.'barbarian.jpg" alt="Barbarian">');
+ echo anchor($urlPath."/bard", $imgPath.'bard.png" alt="Bard">');
+ echo anchor($urlPath."/barbarian", $imgPath.'barbarian.png" alt="Barbarian">');
  ?><br /><?php
- echo anchor($urlPath."/cleric", $imgPath.'cleric.jpg" alt="Cleric">');
- echo anchor($urlPath."/druid", $imgPath.'druid.jpg" alt="Druid">');
- echo anchor($urlPath."/sorcerer", $imgPath.'sorcerer.jpg" alt="Sorcerer">');
+ echo anchor($urlPath."/cleric", $imgPath.'cleric.png" alt="Cleric">');
+ echo anchor($urlPath."/druid", $imgPath.'druid.png" alt="Druid">');
+ echo anchor($urlPath."/sorcerer", $imgPath.'sorcerer.png" alt="Sorcerer">');
  ?><br /><?php
- echo anchor($urlPath."/paladin", $imgPath.'paladin.jpg" alt="Paladin">');
- echo anchor($urlPath."/ranger", $imgPath.'ranger.jpg" alt="Ranger">');
+ echo anchor($urlPath."/paladin", $imgPath.'paladin.png" alt="Paladin">');
+ echo anchor($urlPath."/ranger", $imgPath.'ranger.png" alt="Ranger">');
 ?>
 </div>
 
