2e85974b89e37e40b605208ff570c758c8465459
Mike Dunton <dunton@psy.fsu.edu>
2013-04-25 02:24:47 +0400

    added images

--- application/views/dice/index.php
+++ application/views/dice/index.php
@@ -4,13 +4,13 @@
 
 <?php  
  $urlPath = site_url('dice/roll');
- $imgPath = '<img src="'.base_url() .'assets/img/dice';
- echo anchor($urlPath."/4", $imgPath.'4.png" alt="4">');
- echo anchor($urlPath."/6", $imgPath.'6.png" alt="6">');
- echo anchor($urlPath."/8", $imgPath.'8.png" alt="8">');
- echo anchor($urlPath."/10", $imgPath.'10.png" alt="10">');
- echo anchor($urlPath."/12", $imgPath.'12.png" alt="12">');
- echo anchor($urlPath."/20", $imgPath.'20.png" alt="20">');
+ $imgPath = '<img src="'.base_url() .'assets/img/dice/';
+ echo anchor($urlPath."/4", $imgPath.'d4.jpg" alt="4">');
+ echo anchor($urlPath."/6", $imgPath.'d6.jpg" alt="6">');
+ echo anchor($urlPath."/8", $imgPath.'d8.jpg" alt="8">');
+ echo anchor($urlPath."/10", $imgPath.'d10.jpg" alt="10">');
+ echo anchor($urlPath."/12", $imgPath.'d12.jpg" alt="12">');
+ echo anchor($urlPath."/20", $imgPath.'d20.jpg" alt="20">');
 
  echo form_open('dice/roll');
 ?>
