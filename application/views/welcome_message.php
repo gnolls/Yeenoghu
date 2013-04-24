252204f077961cfa220ca259f45a3c7ebc3071f0
Michael Dunton <dunton@cs.fsu.edu>
2013-04-24 12:28:27 -0400

    Changed welcome message

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,11 +1,16 @@
 	<div id="body">
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+	<h1>Welcome to Dungeon Organizer</h1>
+	<p>Dungeon Organizer is an interactive website for players of Dungeons & Dragons 3.5, that will allow for:
+	<ul>
+		<li>Creation/Management of Character Sheets.</li>
 
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/pages/welcome.php</code>
+	    <li>Searching for spells, items, races, classes, feats, etc.</li>
 
-		<p>you should start by reading the <a href="http://ellislab.com/codeigniter/user-guide/">User Guide</a>.</p>
+	    <li>Creation of parties comprised of a Dungeon Master and players.</li>
+
+	    <li>Conversation and long distance play via a Bulletin Board.</li>
+	</ul>
+	</p>
 	</div>
 
 
