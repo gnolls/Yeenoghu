d1a562d7d09607e1b8eb81feb82cf9e0875e35f2
Michael Dunton <dunton@cs.fsu.edu>
2013-04-24 11:20:04 -0400

    Added more to the spells lookup page

--- application/views/spells/index.php
+++ application/views/spells/index.php
@@ -4,7 +4,13 @@
 <?php
  $imgPath = '<img src="'.base_url() .'assets/img/';
  $urlPath = site_url("spells/classes/");
- echo anchor($urlPath."/bard", $imgPath.'bard" alt="An image">');
+ echo anchor($urlPath."/bard", $imgPath.'bard.jpg" alt="Bard">');
+ echo anchor($urlPath."/barbarian", $imgPath.'barbarian.jpg" alt="Barbarian">');
+ echo anchor($urlPath."/cleric", $imgPath.'cleric.jpg" alt="Cleric">');
+ echo anchor($urlPath."/druid", $imgPath.'druid.jpg" alt="Druid">');
+ echo anchor($urlPath."/sorcerer", $imgPath.'sorcerer.jpg" alt="Sorcerer">');
+ echo anchor($urlPath."/paladin", $imgPath.'paladin.jpg" alt="Paladin">');
+ echo anchor($urlPath."/ranger", $imgPath.'ranger.jpg" alt="Ranger">');
 ?>
 </div>
 
