ba60f63b54d672ced59f236baa7d5bace72b5cc5
Michael Dunton <dunton@cs.fsu.edu>
2013-04-24 12:30:31 -0400

    Lists!

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -2,13 +2,13 @@
 	<h1>Welcome to Dungeon Organizer</h1>
 	<p>Dungeon Organizer is an interactive website for players of Dungeons & Dragons 3.5, that will allow for:
 	<ul>
-		<li>Creation/Management of Character Sheets.</li>
+		<li style="list-style-type:none;">Creation/Management of Character Sheets.</li>
 
-	    <li>Searching for spells, items, races, classes, feats, etc.</li>
+	    <li style="list-style-type:none;">Searching for spells, items, races, classes, feats, etc.</li>
 
-	    <li>Creation of parties comprised of a Dungeon Master and players.</li>
+	    <li style="list-style-type:none;">Creation of parties comprised of a Dungeon Master and players.</li>
 
-	    <li>Conversation and long distance play via a Bulletin Board.</li>
+	    <li style="list-style-type:none;">Conversation and long distance play via a Bulletin Board.</li>
 	</ul>
 	</p>
 	</div>
